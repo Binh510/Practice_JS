b026fdd31db5eb28293263970d2087da4754c7e4
Binh Nguyen <tienbinhnguyen84@gmail.com>
2021-08-18 00:39:11 -0400

    I practice the algorism of array.

--- arrayDemo.js
+++ arrayDemo.js
@@ -8,15 +8,9 @@
     6. declare new array (called listOfIndex) to store index of  max negative numbers if the arraySample has many maxNegative number.
     7. search arraySample from indexOfMaxNegative, if arraySample[i] = arraySample[indexOfMaxNegative]
         add i in listOfIndex.
-
-
-
-
-
-
  */
 
-var a = [4, 16, 5, 12, 5, 9, -3, -15, 6, 8];
+var a = [16, 4, 16, 5, 12, 5, 9, -3, 15, 16, 8];
 var indexOfMaxNegative = -1;
 for (var i = 0; i < a.length; i++) {
     if (a[i] < 0) {
@@ -39,7 +33,31 @@ else {
             listOfIndex.push(i)
         }
     }
-
     console.log('phan tu am lon nhat la ', a[indexOfMaxNegative], ' co index = ', listOfIndex);
 }
+
+// Tinh tong cac so le trong mang
+var total = 0;
+for (var item of a) {
+    if (item % 2 != 0) {
+        total += item;
+    }
+}
+console.log('tong cac so le trong mang la: ', total);
+
+// Tim so lon nhat trong mang a va xuat chi so tai do
+var indexMax = 0;
+for (var i = 1; i < a.length; i++) {
+    if (a[i] > a[indexMax]) {
+        indexMax = i;
+    }
+}
+var indexList = [];
+for (var i = indexMax; i < a.length; i++) {
+    if (a[i] === a[indexMax]) {
+        indexList.push(i);
+    }
+}
+console.log('So lon nhat trong mang la: ', a[indexMax]);
+console.log('Chi so cua so lon nhat la', indexList);
 
